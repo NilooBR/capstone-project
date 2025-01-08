035c072a2dd3bc8f771bcfc72c2ece7f7db97c9e
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2025-01-08 16:30:21 +0100

    added style for initiative card and list container

--- Components/InitiativeCard/InitiativeCard.jsx
+++ Components/InitiativeCard/InitiativeCard.jsx
@@ -1,17 +1,30 @@
+import styled from "styled-components";
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  border-radius: 10px;
+`;
+
 export default function InitiativeCard({ title, tags, deadline }) {
-  return(<>
-    <div>
-      <h3>Initiative {title}</h3>
-      <div>
-        {tags.map((tag, index) => (
-          <span key={index}>{tag}</span>
-        ))}
-      </div>
-      <p>Deadline: {deadline}</p>
-      <div>
-      <button>Update</button>
-      <button>Delete</button>
-      </div>
-    </div>
-  </>)
+  return (
+    <>
+      <Card>
+        <h3>Initiative {title}</h3>
+        <div>
+          {tags.map((tag, index) => (
+            <span key={index}>{tag}</span>
+          ))}
+        </div>
+        <p>Deadline: {deadline}</p>
+        <div>
+          <button>Update</button>
+          <button>Delete</button>
+        </div>
+      </Card>
+    </>
+  );
 }
--- Components/InitiativeList/InitiativeList.jsx
+++ Components/InitiativeList/InitiativeList.jsx
@@ -1,20 +1,30 @@
 import { useState } from "react";
 import InitiativeCard from "../InitiativeCard/InitiativeCard";
 import { initialData } from "@/lib/initialData";
+import styled from "styled-components";
+
+const ListContainer = styled.div`
+  display: grid;
+  gap: 16px;
+  padding: 16px;
+  grid-template-columns: 1fr 1fr;
+`;
 
 export default function InitiativeList() {
   const [initiatives, setInitiatives] = useState(initialData);
 
   return (
     <>
-      {initiatives.map((initiative) => (
-        <InitiativeCard
-          key={initiative.id}
-          title={initiative.title}
-          tags={initiative.tags}
-          deadline={initiative.deadline}
-        />
-      ))}
+      <ListContainer>
+        {initiatives.map((initiative) => (
+          <InitiativeCard
+            key={initiative.id}
+            title={initiative.title}
+            tags={initiative.tags}
+            deadline={initiative.deadline}
+          />
+        ))}
+      </ListContainer>
     </>
   );
 }
