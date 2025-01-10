554ffa5c6b7bb1763f50fbcb3a310b9ce35862af
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2025-01-10 13:16:54 +0100

    added cancel and create button with styling

--- pages/initiatives/create.js
+++ pages/initiatives/create.js
@@ -1,12 +1,15 @@
+import { useRouter } from "next/router";
 import styled from "styled-components";
 
 const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
   padding: 20px;
   max-width: 600px;
   margin: 0 auto;
+  min-height: 100vh;
 `;
 
 const StyledForm = styled.form`
@@ -24,10 +27,46 @@ const Label = styled.label`
   gap: 4px;
 `;
 
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 10px;
+  margin-bottom: 20px;
+  width: 100%;
+`;
+const Button = styled.div`
+  display: inline-block;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  border-radius: 5px;
+  background-color: #6c757d;
+  color: black;
+  font-weight: bold;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #5a6268;
+  }
+`;
+
 export default function CreateInitiative() {
+  const router = useRouter();
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onAddEntry(data);
+    event.target.reset();
+  }
+
+  const handleCancel = () => {
+    router.push("/");
+  };
+
   return (
     <FormContainer>
-      <StyledForm>
+      <StyledForm onSubmit={handleSubmit}>
         <Label htmlFor="title">Initiative Title</Label>
         <input name="title" type="text" required></input>
         <Label htmlFor="description">Description</Label>
@@ -37,6 +76,10 @@ export default function CreateInitiative() {
         <Label htmlFor="tags">Tags (comma-separated)</Label>
         <input name="tags" type="text"></input>
       </StyledForm>
+      <ButtonGroup>
+        <Button onClick={handleCancel}>Cancel</Button>
+        <Button type="submit">Create</Button>
+      </ButtonGroup>
     </FormContainer>
   );
 }
