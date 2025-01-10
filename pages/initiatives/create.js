b73ec8a1723ada45971b7fbc71b3c2798a37d0dc
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2025-01-10 12:34:17 +0100

    added style to form container and labels

--- pages/initiatives/create.js
+++ pages/initiatives/create.js
@@ -1,24 +1,42 @@
 import styled from "styled-components";
 
-const StyledForm = styled.form`
+const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
+  align-items: center;
   padding: 20px;
   max-width: 600px;
   margin: 0 auto;
 `;
 
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 100%;
+  gap: 16px;
+`;
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  font-size: 10px;
+  font-weight: bold;
+  gap: 4px;
+`;
+
 export default function CreateInitiative() {
   return (
-    <StyledForm>
-      <label htmlFor="title">Initiative Title</label>
-      <input name="title" type="text" required></input>
-      <label htmlFor="description">Description</label>
-      <textarea name="description" required></textarea>
-      <label htmlFor="deadline">Deadline</label>
-      <input name="deadline" type="date" required></input>
-      <label htmlFor="tags">Tags (comma-separated)</label>
-      <input name="tags" type="text"></input>
-    </StyledForm>
+    <FormContainer>
+      <StyledForm>
+        <Label htmlFor="title">Initiative Title</Label>
+        <input name="title" type="text" required></input>
+        <Label htmlFor="description">Description</Label>
+        <textarea name="description" required></textarea>
+        <Label htmlFor="deadline">Deadline</Label>
+        <input name="deadline" type="date" required></input>
+        <Label htmlFor="tags">Tags (comma-separated)</Label>
+        <input name="tags" type="text"></input>
+      </StyledForm>
+    </FormContainer>
   );
 }
