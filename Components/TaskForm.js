c02aa8f0ed38429aadb38af241a2e60f8cb60d55
Niloofar Rashvanloo <niloo.bagherinezhad@gmail.com>
2025-01-28 13:54:35 +0100

    deleted extra comment

--- Components/TaskForm.js
+++ Components/TaskForm.js
@@ -54,7 +54,7 @@ export default function TaskForm({ isEditMode = false, initiativeId, taskId }) {
         });
       }
 
-      mutate(); // Revalidate data
+      mutate();
       router.push(`/initiatives/${initiativeId}`);
     } catch (err) {
       console.error("Error saving task:", err);
